e9362a727a69856eb7899b8159eda6a96ac353e3
De'Jour Ford <dejourford@my.unt.edu>
2023-02-28 23:52:37 -0600

    add 2 players to teams array

---
+++ js/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+app = express()
+
+const teams = {
+    'Atlanta Hawks': {
+        'name': 'Trae Young',
+        'img': 'https://cdn.nba.com/headshots/nba/latest/1040x760/1629027.png'
+    },
+
+    'Boston Celtics': {
+        'name': 'Jayson Tatum',
+        'img': 'https://cdn.nba.com/headshots/nba/latest/1040x760/1628369.png'
+    },
+}
